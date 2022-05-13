3b3ae22616d82975dfe9367f192096da2e636ded
Kazuki Hasegawa <hasse09052@gmail.com>
2022-05-14 02:29:40 +0900

    本のサンプルコードを実行

--- Ruby/lib/fizz_buzz.rb
+++ Ruby/lib/fizz_buzz.rb
@@ -1,6 +1,10 @@
 def fizz_buzz(n)
-  if n % 3 == 0
+  if n % 15 == 0
+    'FizzBuzz'
+  elsif n % 3 == 0
     'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
   else
     n.to_s
   end
@@ -8,3 +12,8 @@ end
 
 puts fizz_buzz(1)
 puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(15)
